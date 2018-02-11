1ad2e3983362ca9eff56d9fa7919203abc13fd1a
S Sohail Ur Rahman  <ssohailmails@gmail.com>
2018-02-12 00:24:37 +0530

    Initial Commit

---
+++ OnlineTask/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnlineTask
+//
+//  Created by SOHAIL UR RAHMAN on 12/02/18.
+//  Copyright © 2018 SOHAIL UR RAHMAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnlineTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OnlineTask
+//
+//  Created by SOHAIL UR RAHMAN on 12/02/18.
+//  Copyright © 2018 SOHAIL UR RAHMAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
