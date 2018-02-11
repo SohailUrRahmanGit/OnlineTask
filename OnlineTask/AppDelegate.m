50e15409c6e8a1ccc8a97214f532332440b23387
S Sohail Ur Rahman  <ssohailmails@gmail.com>
2018-02-12 00:42:32 +0530

    Remote initial commit

--- OnlineTask/AppDelegate.m
+++ OnlineTask/AppDelegate.m
@@ -16,7 +16,6 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    NSLog(@"For Git initial Commit");
     // Override point for customization after application launch.
     return YES;
 }
